fb2f2adfe58d68c223d05ef04c53278efb0b7c68
cheerfulperson <yagor.baradavkin@gmail.com>
2022-07-17 22:31:43 +0300

    feat: add tracks handler

---
+++ src/modules/tracks/services/tracks.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
+import { Track, TrackDto, TracksResponse } from '../models/tracks.model';
+
+@Injectable()
+export class TracksService {
+  private tracks: Track[] = [];
+
+  public async getAllTracks(
+    limit?: number,
+    offset = 0,
+  ): Promise<TracksResponse> {
+    return {
+      items: limit
+        ? this.tracks.slice(limit * offset, limit * (offset + 1))
+        : this.tracks,
+      limit: limit ?? null,
+      total: this.tracks.length,
+      offset,
+    };
+  }
+
+  public async addOneTrack(trackInfo: TrackDto) {
+    const track = {
+      id: v4(),
+      ...trackInfo,
+    };
+    this.tracks.push(track);
+    return track;
+  }
+
+  public async getOneTrack(id: string) {
+    const track = this.tracks.find((track) => track.id === id);
+    return track;
+  }
+
+  public async updateTrack(id: string, body: TrackDto) {
+    return new Promise((res, rej) => {
+      this.tracks.forEach((track) => {
+        if (track.id === id) {
+          track.name = body.name;
+          track.duration = body.duration;
+          track.albumId = body.albumId;
+          track.artistId = body.artistId;
+          res(track);
+        }
+      });
+      rej();
+    });
+  }
+
+  public deleteOneTrack(id: string) {
+    this.tracks = this.tracks.filter((track) => track.id !== id);
+  }
+
+  public checkTrackInfo(track: TrackDto): string[] | null {
+    const messages: string[] = [];
+
+    if (typeof track.name !== 'string') {
+      messages.push('name is required field');
+    }
+    if (typeof track.duration !== 'number') {
+      messages.push('duration is required field');
+    }
+    return messages.length === 0 ? null : messages;
+  }
+}
--- src/modules/tracks/tracks.module.ts
+++ src/modules/tracks/tracks.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TracksService } from './services/tracks.service';
+import { TracksController } from './tracks.controller';
 
-@Module({})
+@Module({
+  providers: [TracksService],
+  controllers: [TracksController],
+  exports: [TracksService],
+})
 export class TracksModule {}
